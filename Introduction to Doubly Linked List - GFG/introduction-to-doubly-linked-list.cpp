99bc072d56a86d437f3415a914736757a5bfd1ae
Gaurav Kumar <103420886+gauravai2025@users.noreply.github.com>
2023-12-28 07:17:29 +0530

    Added solution - LeetHub

---
+++ Introduction to Doubly Linked List - GFG/introduction-to-doubly-linked-list.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    Node* prev;
+    int data;
+    Node* next;
+ 
+    Node()
+    { 
+        prev = NULL; 
+        data = 0;
+        next = NULL;
+    }
+    
+    Node(int value)
+    { 
+        prev = NULL; 
+        data = value;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*
+class Node{
+public:
+    Node* prev;
+    int data;
+    Node* next;
+ 
+    Node()
+    { 
+        prev = NULL; 
+        data = 0;
+        next = NULL;
+    }
+    
+    Node(int value)
+    { 
+        prev = NULL; 
+        data = value;
+        next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructDLL(vector<int>& arr) {
+       
+        Node* head=NULL;
+        Node* curr=NULL;
+        
+        for(auto & it:arr){
+            // firts node 
+            if(head==NULL){
+                Node * temp=new Node(it);
+                head=temp;
+                 curr=temp;
+            }
+            
+            else{
+             Node * temp=new Node(it);
+             curr->next=temp;
+             temp->prev=curr;
+             curr=temp;
+             
+            }
+        }
+        
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        Node* ans = obj.constructDLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
